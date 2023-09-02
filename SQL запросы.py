814791170593586dc05727c785edcb0f10484dcd
ЕленаБронникова555 <elenarai555@gmail.com>
2023-09-02 17:07:18 +0300

    My first diplom

---
+++ SQL запросы.py
@@ -0,0 +1,13 @@
+SELECT Couriers.login, COUNT(Orders.id) AS num_orders_in_delivery
+FROM "Couriers"
+JOIN "Orders" ON "Couriers".id = "Orders"."courierId"
+WHERE "Orders"."inDelivery" = true
+GROUP BY "Couriers".login;
+
+SELECT track
+FROM "Orders"
+    WHEN finished = true THEN 2
+    WHEN cancelled = true THEN - 1
+    WHEN "inDelivery" = true THEN 1
+    ELSE 0
+  END AS status;
